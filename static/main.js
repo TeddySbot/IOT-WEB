916c75699a61d689800d837f3439e7430b102398
lteddy <teddy.lemoal@ynov.com>
2025-11-12 17:18:00 +0100

    double facteur

---
+++ static/main.js
@@ -0,0 +1,25 @@
+// Connexion au broker EMQX via WebSocket
+const client = mqtt.connect('wss://broker.emqx.io:8084/mqtt');
+
+client.on('connect', () => {
+  console.log('✅ Connecté au broker MQTT');
+  client.subscribe('VHT/Température/Salon', err => {
+    if (!err) console.log('Abonné au topic VHT/Température/Salon');
+  });
+});
+
+// Quand un message arrive
+client.on('message', (topic, message) => {
+  const msg = message.toString();
+
+  try {
+    // On parse le message JSON
+    const data = JSON.parse(msg);
+
+    // On met à jour les éléments HTML
+    document.getElementById('temp').innerText = data.Celsius;
+    document.getElementById('hum').innerText = data.Humidité;
+  } catch (e) {
+    console.error('Erreur parsing JSON:', e, 'Message reçu:', msg);
+  }
+});
